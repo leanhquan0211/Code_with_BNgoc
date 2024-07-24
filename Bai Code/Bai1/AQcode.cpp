181fe42a2f588f955c05bbc73ee08bee0192d0cc
lekuan_11 <anhquan02114869@gmail.com>
2024-07-24 14:22:01 +0700

    upload code

--- Bai Code/Bai1/AQcode.cpp
+++ Bai Code/Bai1/AQcode.cpp
@@ -1,6 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int m;
+    cin >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] + a[j] == m)
+            {
+                cout << "[" << i << ", " << j << "]" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "NO";
 }
