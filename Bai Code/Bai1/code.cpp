c77eaec22abddc1cfc97979df16bf0b255092832
lekuan_11 <anhquan02114869@gmail.com>
2024-07-24 15:11:11 +0700

    upload code.cpp bài 1

--- Bai Code/Bai1/code.cpp
+++ Bai Code/Bai1/code.cpp
@@ -1,6 +1,44 @@
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // Trả về vector rỗng nếu không tìm thấy cặp nào
     }
 };
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n); // Khởi tạo vector a với kích thước n
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int m;
+    cin >> m;
+    vector<int> result = Solution().twoSum(a, m);
+    cout << "[";
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i];
+        if (i < result.size() - 1)
+            cout << ",";
+    }
+    cout << "]";
+}
