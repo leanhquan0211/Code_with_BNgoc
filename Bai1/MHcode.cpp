84beea1ae2bf44373ab5e0eead08bcbae7cf26ca
unknown <minh-hieu1109>
2024-07-24 12:00:33 +0700

    add MHcode.cpp

---
+++ Bai1/MHcode.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n, m;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	cin >> m;
+	int t = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n; j++) {
+			t += a[j];
+			if (t == m) {
+				cout << "[" << i << "," << j << "]";
+				return 0;
+			}
+		}
+		t = 0;
+	}
+	cout << -1;
+}
